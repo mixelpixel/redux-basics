8a646f0ccae30a7de8953a80746418ebfcbf044b
mixelpixel <mixelpix@mac.com>
2017-07-28 15:58:16 -0400

    file structure

--- todo/src/components/App.js
+++ todo/src/components/App.js
@@ -1,10 +1,22 @@
 import React from 'react'
+// import React, { Component } from 'react'; // <--- don't need cuz here App is function, not class extending component
 import Footer from './Footer'
 import AddTodo from '../containers/AddTodo'
 import VisibleTodoList from '../containers/VisibleTodoList'
+import logo from '../art/logo.svg';
+import '../css/App.css';
 
 const App = () => (
   <div>
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </div>
     <AddTodo />
     <VisibleTodoList />
     <Footer />
--- todo/src/index.js
+++ todo/src/index.js
@@ -13,6 +13,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
+import './css/index.css';
 
 let store = createStore(todoApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
